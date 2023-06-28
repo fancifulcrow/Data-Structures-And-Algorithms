ea69574c385d64a303e68b242b2fa919597ffa39
Ngozi <124025329+fancifulcrow@users.noreply.github.com>
2023-06-28 17:42:30 +0100

    Completed GraphDS class

--- src/DataStructures/GraphDS.java
+++ src/DataStructures/GraphDS.java
@@ -2,37 +2,77 @@ package DataStructures;
 
 public class GraphDS<E> {
     // Graphs in programming are usually represented by their Adjacency Matrix
-    
-    private HashTableDS<E, LinkedListDS<E>> adjacencyMap; // The Adjacency Matrix
+    // The Adjacency Matrix here is implemented as a Hash Table of Linked Lists
+    private HashTableDS<E, LinkedListDS<E>> adjacencyMap; // The Adjacency Matrix (or rather Map in this case)
     private boolean isDirected;
 
     // Constructor of GraphDS to create a directed or undirected graph
-    public GraphDS(boolean isDirected) {
+    public GraphDS(boolean isDirected){
         this.isDirected = isDirected;
         adjacencyMap = new HashTableDS<>();
     }
 
-    // Add a new vertex
-    public void addVertex(E vertex) {
+    // Add a new vertex to the graph
+    public void addVertex(E vertex){
         adjacencyMap.insert(vertex, new LinkedListDS<>());
     }
 
-    // Add a new edge
-    public void addEdge(E source, E destination) {
+    // Add a new edge to the graph
+    public void addEdge(E source, E destination){
         // Adds the source vertex to the graph if it does not exist
-        if (!adjacencyMap.containsKey(source)) {
+        if (!adjacencyMap.containsKey(source)){
             addVertex(source);
         }
         // Adds the destination vertex to the graph if it does not exist
-        if (!adjacencyMap.containsKey(destination)) {
+        if (!adjacencyMap.containsKey(destination)){
             addVertex(destination);
         }
 
         // adds the edge to the graph
         adjacencyMap.get(source).insert(destination);
         // Symmetrize if the graph is undirected
-        if (!isDirected) {
+        if (!isDirected){
             adjacencyMap.get(destination).insert(source);
+        }
+    }
+
+    // Remove a vertex from the graph
+    public void removeVertex(E vertex){
+        // Check if the vertex exists in the graph
+        if (!adjacencyMap.containsKey(vertex)){
+            return; // If vertex does not exist, do nothing
+        }
+
+        // Remove all edges connected to the given vertex
+        // Iterate over the Adjacency Map
+        for (E v : adjacencyMap.keySet()){
+            // For each vertex, get its set of neighbors
+            LinkedListDS<E> neighbors = adjacencyMap.get(v);
+            // From the set of neigbors, remove the given vertex if found, else continue
+            try {
+                neighbors.remove(vertex);
+            } catch (IllegalArgumentException iae){
+                continue;
+            }
+        }
+
+        // Remove the given vertex from the adjacency map
+        adjacencyMap.remove(vertex);
+    }
+
+    // Removes an edge from the graph
+    public void removeEdge(E source, E destination){
+        // Check if both source and destination vertices exist in the graph
+        if (!adjacencyMap.containsKey(source) || !adjacencyMap.containsKey(destination)){
+            // Do nothing if either vertex is not present in the graph
+            return;
+        }
+
+        // Remove the edge from the source vertex
+        adjacencyMap.get(source).remove(destination);
+        // If the graph is undirected, remove the corresponding edge from the destination vertex
+        if (!isDirected){
+            adjacencyMap.get(destination).remove(source);
         }
     }
 
@@ -41,8 +81,56 @@ public class GraphDS<E> {
         return adjacencyMap.size();
     }
 
-    // Checks whethere the graph is directed or not
-    public boolean isDirectedGraph() {
+    // Returns the number of edges in the graph
+    public int edgesCount(){
+        int count = 0;
+        // Iterate over the hash table
+        for (E v : adjacencyMap.keySet()){
+            // Add the size of each linked list to the count
+            count += adjacencyMap.get(v).size();
+        }
+        if (!isDirected){
+            count /= 2; // Divide by 2 for undirected graphs
+        }
+        return count;
+    }
+
+    // Checks whether a given vertex exists in the graph
+    public boolean hasVertex(E vertex){
+        return adjacencyMap.containsKey(vertex);
+    }
+
+    // Checks whether a given edge exists in the graph
+    public boolean hasEdge(E source, E destination){
+        // Checks if the source vertex exists in te graph
+        if (!adjacencyMap.containsKey(source)){
+            return false;
+        }
+        // Checks if the destination exists in the set of neighbors of the source vertex
+        return adjacencyMap.get(source).containsValue(destination);
+    }
+
+    // Checks whether the graph is directed or undirected
+    public boolean isDirectedGraph(){
         return isDirected;
+    }
+
+    // Prints out the Adjacency Map
+    public void print(){
+        // Iterate over the Adjavency Map
+        for (E v : adjacencyMap.keySet()){
+            // For each vertex, get its set of neighbors
+            LinkedListDS<E> neighbors = adjacencyMap.get(v);
+            
+            // Print out each vertex and its neighbors
+            System.out.print(v + " : ");
+            neighbors.print();
+            System.out.println();
+        }
+    }
+
+    // Clear out the graph
+    public void clear(){
+        adjacencyMap.clear();
     }
 }
--- src/Testing.java
+++ src/Testing.java
@@ -4,7 +4,26 @@ public class Testing {
     public static void main(String[] args) {
         GraphDS<Integer> graphDS = new GraphDS<>(false);
 
-        graphDS.addEdge(1, 2);
+        graphDS.addEdge(1, 2);        
+        graphDS.addEdge(2, 3);
+        graphDS.addEdge(3, 4);
+        graphDS.addEdge(4, 5);
+        graphDS.addEdge(5, 6);
+
+        graphDS.removeVertex(4);
+        graphDS.removeEdge(5, 6);
+
         System.out.println(graphDS.verticesCount());
+
+        graphDS.print();
+
+        GraphDS<String> graphDS2 = new GraphDS<>(false);
+        graphDS2.addVertex("Hello World");
+        graphDS2.addEdge("null", "Hey People");
+        graphDS2.addEdge("null", "Hello World");
+        graphDS2.print();
+
+        System.out.println(graphDS.hasEdge(2, 1));
+        System.out.println(graphDS2.edgesCount());
     }
 }
